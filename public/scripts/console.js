2721743b43f7aabd9f6c9aa1448e89bcb1b59469
John Mettraux <jmettraux@gmail.com>
2025-01-01 10:58:42 +0900

    Scaffold public/scripts/

---
+++ public/scripts/console.js
@@ -0,0 +1,23 @@
+
+//
+// console.js
+
+var Console = (function() {
+
+  "use strict";
+
+  let self = this;
+
+  //
+  // protected functions
+
+  //
+  // public functions
+
+  //
+  // over.
+
+  return this;
+
+}).apply({}); // end Console
+
---
+++ public/scripts/koukai.js
@@ -0,0 +1,23 @@
+
+//
+// koukai.js
+
+var Koukai = (function() {
+
+  "use strict";
+
+  let self = this;
+
+  //
+  // protected functions
+
+  //
+  // public functions
+
+  //
+  // over.
+
+  return this;
+
+}).apply({}); // end Koukai
+
