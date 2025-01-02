7146a077a92000925af4de53feb6a6032402c8da
John Mettraux <jmettraux@gmail.com>
2025-01-02 10:15:10 +0900

    Implement GoBoard attributeChangedCallback

--- public/scripts/go_board.js
+++ public/scripts/go_board.js
@@ -2,7 +2,46 @@
 //
 // go_board.js
 
-class GoBoard extends HTMLDivElement {
+class DivComponent extends HTMLDivElement {
+
+  //
+  // private methods
+
+  //
+  // "protected" methods
+
+  //
+  // public methods
+
+  attributeChangedCallback(name, v0, v1) {
+
+    let ns = name.split('-').slice(1);
+    if (ns[0] === 'sg') ns = ns.slice(1);
+    let n = ns.map(e => H.cap(e)).join('');
+      //
+    let fn = `_on${n}AttChange`;
+    let f = this[fn];
+
+    if (typeof f === 'function') return f.bind(this)(name, v0, v1);
+    throw new Error(`No \`${fn}\` function in ${this.constructor.name} class`);
+  }
+}
+
+class GoBoard extends DivComponent {
+
+  //
+  // private methods
+
+  //
+  // "protected" methods
+
+  _onKoukaiSizeAttChange(name, v0, v1) {
+
+clog('att', name, '->', v1);
+  }
+
+  //
+  // public methods
 
   //constructor() {
   //  super();
@@ -15,12 +54,10 @@ class GoBoard extends HTMLDivElement {
   }
 
   static get observedAttributes() {
-    return [ 'data-koukai-size' ]; }
-
-  attributeChangedCallback(name, v0, v1) {
-
-clog('attributeChangedCallback()', name, v0, v1);
-  }
+    return [
+      'data-koukai-engine', 'data-koukai-engine-id',
+      'data-koukai-size', 'data-koukai-sound'
+        ]; }
 }
 
 customElements.define('go-board', GoBoard, { extends: 'div' });
